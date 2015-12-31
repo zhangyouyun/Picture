277b6e34d609e88f7cd3a81cdca0cd9b0fd69499
zhangyoyun <744120141@qq.com>
2015-12-31 11:16:01 +0800

    Initial commit

---
+++ app/src/main/java/mygson/android/com/test/ViewFactory.java
@@ -0,0 +1,24 @@
+package mygson.android.com.test;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+/**
+ * ImageView创建工厂
+ */
+public class ViewFactory {
+
+	/**
+	 * 获取ImageView视图的同时加载显示url
+	 * 
+	 * @param
+	 * @return
+	 */
+	public static ImageView getImageView(Context context, String url) {
+		ImageView imageView = (ImageView)LayoutInflater.from(context).inflate(
+				R.layout.view_banner, null);
+		ImageLoader.getInstance().displayImage(url, imageView);
+		return imageView;
+	}
+}
