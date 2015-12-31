b6d9924c3ac47dedcfa7e146da75f739030dc9c8
zhangyoyun <744120141@qq.com>
2015-12-31 13:34:35 +0800

    picture

--- app/src/main/java/mygson/android/com/test/MainActivity.java
+++ app/src/main/java/mygson/android/com/test/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private List<ImageView> views = new ArrayList<ImageView>();
     private List<ADInfo> infos = new ArrayList<ADInfo>();
     private CycleViewPager cycleViewPager;
-
+//android:drawableLeft="@mipmap/back"
     private String[] imageUrls = {"http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg",
             "http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg",
             "http://pic18.nipic.com/20111215/577405_080531548148_2.jpg",
